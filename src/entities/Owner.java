357ba72d6f3cbaf9d6c227407d9757a3316eb09e
inesboukhizzou <ines.boukhizzou@ig2i.centralelille.fr>
2025-12-14 17:48:46 +0100

    added the Owner class and the connexion to the JPA DB

--- src/entities/Owner.java
+++ src/entities/Owner.java
@@ -1,4 +1,92 @@
 package entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "owner")
 public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    // Constructors
+    public Owner() {
+    }
+
+    public Owner(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Owner(String firstName, String lastName, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- src/utils/JPAUtil.java
+++ src/utils/JPAUtil.java
@@ -1,4 +1,23 @@
 package utils;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JPAUtil {
+    private static EntityManagerFactory emf = null;
+
+    // Singleton : une seule instance pour toute l'application
+    public static EntityManagerFactory getEntityManagerFactory() {
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory("Auto2IPU");
+            // "Auto2IPU" doit correspondre au nom dans persistence.xml
+        }
+        return emf;
+    }
+
+    public static void close() {
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
 }
