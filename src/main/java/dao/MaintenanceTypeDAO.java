5a30254fc8f4863d1f45f080d75285e899a39ada
inesboukhizzou <ines.boukhizzou@ig2i.centralelille.fr>
2026-01-06 10:22:41 +0100

    maintenanceTypeDAO changed

--- src/main/java/dao/MaintenanceTypeDAO.java
+++ src/main/java/dao/MaintenanceTypeDAO.java
@@ -1,16 +1,6 @@
 package dao;
 
 import entities.MaintenanceType;
-<<<<<<< HEAD
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
-import utils.JPAUtil;
-import java.util.*;
-
-public class MaintenanceTypeDAO {
-
-    public void save(MaintenanceType maintenanceType) {
-=======
 import entities.InterventionType;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
@@ -21,7 +11,6 @@ import java.util.List;
 public class MaintenanceTypeDAO {
 
     public void create(MaintenanceType maintenanceType){
->>>>>>> aa620b8d4cf27157d9c24703e53e14823a3e8688
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
         EntityTransaction et = em.getTransaction();
         try{
@@ -29,15 +18,6 @@ public class MaintenanceTypeDAO {
             em.persist(maintenanceType);
             et.commit();
         }
-<<<<<<< HEAD
-        catch(RuntimeException re){
-            if(et.isActive()){
-                et.rollback();
-            }
-            throw re;
-        }
-        finally {
-=======
         catch(RuntimeException e){
             if(et.isActive()){
                 et.rollback();
@@ -45,15 +25,10 @@ public class MaintenanceTypeDAO {
             throw e;
         }
         finally{
->>>>>>> aa620b8d4cf27157d9c24703e53e14823a3e8688
             em.close();
         }
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> aa620b8d4cf27157d9c24703e53e14823a3e8688
     public MaintenanceType findById(Long id){
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
         try{
@@ -64,26 +39,6 @@ public class MaintenanceTypeDAO {
         }
     }
 
-<<<<<<< HEAD
-
-
-    public void removeMaintenanceType(Long id){
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        EntityTransaction et = em.getTransaction();
-        try{
-            MaintenanceType maintenanceType = em.find(MaintenanceType.class, id);
-            et.begin();
-            em.remove(maintenanceType);
-            et.commit();
-        }
-        catch(RuntimeException re){
-            if(et.isActive()){
-                et.rollback();
-            }
-            throw re;
-        }
-        finally {
-=======
     public List<MaintenanceType> findAll(){
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
         try{
@@ -93,28 +48,24 @@ public class MaintenanceTypeDAO {
             ).getResultList();
         }
         finally{
->>>>>>> aa620b8d4cf27157d9c24703e53e14823a3e8688
             em.close();
         }
     }
 
-<<<<<<< HEAD
-=======
     public List<MaintenanceType> findByInterventionType(InterventionType interventionType){
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
         try{
             return em.createQuery(
-                    "SELECT m FROM MaintenanceType m WHERE m.interventionType = :interventionType",
-                    MaintenanceType.class
-            )
-            .setParameter("interventionType", interventionType)
-            .getResultList();
+                            "SELECT m FROM MaintenanceType m WHERE m.interventionType = :interventionType",
+                            MaintenanceType.class
+                    )
+                    .setParameter("interventionType", interventionType)
+                    .getResultList();
         }
         finally{
             em.close();
         }
     }
->>>>>>> aa620b8d4cf27157d9c24703e53e14823a3e8688
 
     public void setMaxDuration(Long id, int maxDuration){
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
@@ -125,15 +76,6 @@ public class MaintenanceTypeDAO {
             maintenanceType.setMaxDuration(maxDuration);
             et.commit();
         }
-<<<<<<< HEAD
-        catch(RuntimeException re){
-            if(et.isActive()){
-                et.rollback();
-            }
-            throw re;
-        }
-        finally {
-=======
         catch(RuntimeException e){
             if(et.isActive()){
                 et.rollback();
@@ -141,15 +83,10 @@ public class MaintenanceTypeDAO {
             throw e;
         }
         finally{
->>>>>>> aa620b8d4cf27157d9c24703e53e14823a3e8688
             em.close();
         }
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> aa620b8d4cf27157d9c24703e53e14823a3e8688
     public void setMaxMileage(Long id, int maxMileage){
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
         EntityTransaction et = em.getTransaction();
@@ -159,40 +96,16 @@ public class MaintenanceTypeDAO {
             maintenanceType.setMaxMileage(maxMileage);
             et.commit();
         }
-<<<<<<< HEAD
-        catch(RuntimeException re){
-            if(et.isActive()){
-                et.rollback();
-            }
-            throw re;
-        }
-        finally {
-            em.close();
-        }
-    }
-
-    public List<MaintenanceType> findAll(){
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        try{
-            return em.createQuery(
-                    "SELECT r FROM MaintenanceType r",
-                    MaintenanceType.class
-            ).getResultList();
-=======
         catch(RuntimeException e){
             if(et.isActive()){
                 et.rollback();
             }
             throw e;
->>>>>>> aa620b8d4cf27157d9c24703e53e14823a3e8688
         }
         finally{
             em.close();
         }
     }
-<<<<<<< HEAD
-}
-=======
 
     public void remove(Long id){
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
@@ -214,4 +127,3 @@ public class MaintenanceTypeDAO {
         }
     }
 }
->>>>>>> aa620b8d4cf27157d9c24703e53e14823a3e8688
